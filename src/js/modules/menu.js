5cadea40f7ae127e4b500f24352a085fb716410c
Abdukulov Talgat <abdukulov.talgat@gmail.com>
2022-06-05 21:27:46 +0500

    added background for header when scroll in desktop version

--- src/js/modules/menu.js
+++ src/js/modules/menu.js
@@ -1,6 +1,23 @@
 // **************** Burger menu ****************
+const burgerBreakpoint = 991.98;
 
-export default function initMenu(){
+export default function initMenu() {
+    initBurger();
+
+    if(window.innerWidth > burgerBreakpoint){
+        window.addEventListener('scroll', onScroll);
+    }
+
+    window.addEventListener('resize', (evt) => {
+        if (window.innerWidth > burgerBreakpoint) {
+            window.addEventListener('scroll', onScroll);
+            return;
+        }
+        window.removeEventListener('scroll', onScroll);
+    });
+}
+
+function initBurger() {
     const burger = document.querySelector('.menu__burger');
 
     burger.addEventListener('click', () => {
@@ -8,3 +25,14 @@ export default function initMenu(){
         document.documentElement.classList.toggle('opened')
     });
 }
+
+
+const header = document.querySelector('.header');
+const subMenu = document.querySelector('.sub-menu');
+
+function onScroll(evt) {
+    let bgColor = window.scrollY === 0 ? '' : '#fff';
+    header.style.backgroundColor = subMenu.style.backgroundColor = bgColor;
+}
+
+
